56f08b4723fc1766bf32607c7763d4a82da2a0b7
shryash12 <dr.shradhasrathod@gmail.com>
2021-01-30 12:59:44 +0800

    initial commit for WeatherData

---
+++ app/src/main/java/shradha/com/weatherdata/MainActivity.java
@@ -0,0 +1,14 @@
+package shradha.com.weatherdata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
