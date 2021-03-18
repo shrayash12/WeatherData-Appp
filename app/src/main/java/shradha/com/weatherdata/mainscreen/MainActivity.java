a124e7834e1b06b15bdf8c24006c6a3e4545a3d2
shryash12 <dr.shradhasrathod@gmail.com>
2021-03-18 23:42:23 +0800

    theme changes

--- app/src/main/java/shradha/com/weatherdata/mainscreen/MainActivity.java
+++ app/src/main/java/shradha/com/weatherdata/mainscreen/MainActivity.java
@@ -13,6 +13,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextClock;
 import android.widget.TextView;
@@ -72,7 +74,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
         ((WeatherDataApplication) getApplication()).getWeatherDataComponents().inject(this);
 
 
